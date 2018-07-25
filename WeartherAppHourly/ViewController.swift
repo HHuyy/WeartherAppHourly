a12ae67e7d13a51e9c3b4069ddf59da5f85dc1f3
Đừng xóa <nomnom1204@gmail.com>
2018-07-25 08:10:26 +0700

    Initial Commit

---
+++ WeartherAppHourly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeartherAppHourly
+//
+//  Created by Đừng xóa on 7/25/18.
+//  Copyright © 2018 Đừng xóa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
